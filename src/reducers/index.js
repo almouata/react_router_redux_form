e04f8394712e4718a53fafdea872bed5782e9b49
Abdess <lmouatassime@icloud.com>
2017-12-24 17:52:43 +0000

    redux form and submitting to the api

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,7 +3,8 @@ import { reducer as formReducer } from 'redux-form'
 import PostsReducer from './posts_reducer'
 
 const rootReducer = combineReducers({
-  posts:PostsReducer
+  posts:PostsReducer,
+  form : formReducer
 });
 
 export default rootReducer;
--- src/reducers/posts_reducer.js
+++ src/reducers/posts_reducer.js
@@ -1,11 +1,14 @@
 import _ from 'lodash'
-import { FETCH_POST } from '../actions/index'
+import { FETCH_POST, CREATE_POST } from '../actions/index'
 
 export default function (state = {}, action){
   switch (action.type) {
     case FETCH_POST:
       return _.mapKeys(action.payload.data,'id')
       break;
+      //case CREATE_POST:
+      //  return {}
+      //  break;
     default:
       return state;
 
