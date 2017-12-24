175dc576492d496a02a2c113f448958c9ae219d0
Abdess <lmouatassime@icloud.com>
2017-12-24 10:43:32 +0000

    react route initiation

---
+++ src/components/posts_new.js
@@ -0,0 +1,18 @@
+import React , {Component} from 'react'
+import { connect } from 'react-redux'
+
+
+class PostsNew extends Component {
+  render (){
+    return (
+      <div>
+        Post new
+      </div>
+    )
+
+  }
+
+}
+
+
+export default PostsNew
---
+++ src/reducers/posts_reducer.js
@@ -0,0 +1,13 @@
+import _ from 'lodash'
+import { FETCH_POST } from '../actions/index'
+
+export default function (state = {}, action){
+  switch (action.type) {
+    case FETCH_POST:
+      return _.mapKeys(action.payload.data,'id')
+      break;
+    default:
+      return state;
+
+  }
+}
