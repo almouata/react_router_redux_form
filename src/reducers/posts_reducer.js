152c6013e179581ca9477f4cf58e97957a5f21b7
Abdess <lmouatassime@icloud.com>
2018-01-12 17:19:33 +0000

    finishing the CRUDfor pos app

--- src/reducers/posts_reducer.js
+++ src/reducers/posts_reducer.js
@@ -1,10 +1,14 @@
 import _ from 'lodash'
-import { FETCH_POST, CREATE_POST } from '../actions/index'
+import { FETCH_POST, FETCH_POSTS, CREATE_POST } from '../actions/index'
 
 export default function (state = {}, action){
   switch (action.type) {
-    case FETCH_POST:
+    case FETCH_POSTS:
       return _.mapKeys(action.payload.data,'id')
+      break;
+    case FETCH_POST:
+      const post = action.payload.data
+      return { ...state, [post.id]:post }
       break;
       //case CREATE_POST:
       //  return {}
